dba37d6102611eecc54710192611a2e5fc0a959d
Alex Mikhalev <elias.loker7@gmail.com>
2021-07-16 23:51:08 +0300

    - перевел strings.xml на английский, раз уж написал readme-файл

--- app/src/main/java/com/example/projectstages/base/viewmodel/BaseViewModel.kt
+++ app/src/main/java/com/example/projectstages/base/viewmodel/BaseViewModel.kt
@@ -1,7 +1,5 @@
 package com.example.projectstages.base.viewmodel
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.Dispatchers
